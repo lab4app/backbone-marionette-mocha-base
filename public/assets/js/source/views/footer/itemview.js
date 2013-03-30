c6c27243b03e8c77edcc414d9b56bd079ceecb35
Julio C. Menendez <me@juliocarlosmenendez.com>
2013-03-30 11:35:24 -0600

    When user clicks on footer button, mark it as selected.

--- public/assets/js/source/views/footer/itemview.js
+++ public/assets/js/source/views/footer/itemview.js
@@ -3,11 +3,20 @@ define(function(require) {
 
     var Backbone = require('backbone.all'),
         Mustache = require('mustache'),
-        template = require('text!templates/footer/itemview.html');
+        template = require('text!templates/footer/itemview.html'),
+        $ = require('jquery');
 
     var FooterView = Backbone.Marionette.Layout.extend({
         template: Mustache.compile(template),
-        className: 'bottombar'
+        className: 'bottombar',
+        events: {
+            'click .nav__item a': '_onNavItemClick'
+        },
+
+        _onNavItemClick: function(event) {
+            var parent = $(event.currentTarget).parents('.nav__item');
+            parent.siblings('.is-selected').removeClass('is-selected').end().addClass('is-selected');
+        }
     });
 
     return FooterView;
